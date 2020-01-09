16f04f4ba15c6d605e51703082bbf6c3c3f6b501
krable91 <dydansp@naver.com>
2020-01-10 02:30:05 +0900

    Movie App CSS added

--- src/Movie.js
+++ src/Movie.js
@@ -6,8 +6,8 @@ function Movie({ id, year, title, summary, poster, genres}) {
     return <div className="movie">
         <img src={poster} alt={title} title= {title}/>
         <div className="movie__data">
-            <h3 className="movie__title">{title}</h3>
-            <h5 className="movie__year">{year}</h5>
+            <span className="movie__title">{title}</span>
+            <span className="movie__year">{year}</span>
             <ul className="genres">
                 {genres.map((genre, index) => (
                     <li key={index} className="genres__genre">{genre}</li>
